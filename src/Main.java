c848bab315f702fe2c5e96201c59e69f489f28a3
Mobin <Mobinmojiri1234@gmail.com>
2025-03-18 16:16:20 +0330

    coding main

--- src/Main.java
+++ src/Main.java
@@ -3,30 +3,71 @@ import Library.*;
 
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+
         Library library = new Library();
 
-        library.listBooks();
+
+        try{
+            library.listBooks();
+        }catch (EmptyLibraryException e){
+            System.out.println(e.getMessage());
+        }
+
 
         Book book1 = null;
         Book book2 = null;
         Book book3 = null;
         Book book4 = null;
 
+
+
         book1 = new Book("Java Programming", 300);
-        book2 = new Book("Design Patterns", -3);
-        book3 = new Book(null, 100);
+
+        try{
+            book2 = new Book("Design Patterns", -3);
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+
+        try{
+            book3 = new Book(null, 100);
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+
         book4 = new Book("", 400);
 
+
         library.addBook(book1);
-        library.addBook(book2);
-        library.addBook(book3);
+
+        try {
+            library.addBook(book2);
+        }catch (InvalidBookException e){
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            library.addBook(book3);
+        }catch (InvalidBookException e){
+            System.out.println(e.getMessage());
+        }
+
         library.addBook(book4);
 
         library.listBooks();
 
+
         library.borrowBook("Java Programming");
-        library.returnBook("Clean Code");
+
+        try {
+            library.returnBook("Clean Code");
+        }catch (BookNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+
+
         library.returnBook("Java Programming");
     }
 }
